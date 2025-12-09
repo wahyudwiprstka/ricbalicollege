5e004168e502df37727cce94ae567f36964e8e5a
wahyudwiprstka <wahyudwiprstka@gmail.com>
2025-12-09 16:34:07 +0800

    commit

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -14,6 +14,7 @@ import {
 import NewsPagination from "../components/newsComponents/Pagination";
 import Hero from "../components/Hero";
 import { getTranslations } from "next-intl/server";
+import { getUserLocale } from "@/lib/locale";
 
 export const revalidate = 60;
 interface PageProps {
@@ -29,14 +30,29 @@ export default async function BeritaPage({ searchParams }: PageProps) {
   console.log("Page: " + page);
   const limit = 3;
   const skip = (page - 1) * limit;
+  const locale = await getUserLocale();
 
   // get total news
-  const totalQuery = `count(*[_type == 'news'])`;
+  const totalQueryEN = `count(*[_type == 'newsEN'])`;
+  const totalQueryID = `count(*[_type == 'news'])`;
+  let totalQuery = totalQueryEN;
+  if (locale == "id") {
+    totalQuery = totalQueryID;
+  }
+
   const totalNews = await client.fetch(totalQuery);
   const totalpages = Math.ceil(totalNews / limit);
 
+  if (totalNews == 0) {
+    return (
+      <div className="min-h-[80vh] flex justify-center items-center bg-gray-100 text-2xl font-semibold">
+        {locale == "id" ? "Belum Ada Berita" : "No News Available"}
+      </div>
+    );
+  }
+
   // get news
-  const query = `*[_type == 'news'] | order(publishedAt desc) [${skip}...${
+  const queryID = `*[_type == 'news'] | order(publishedAt desc) [${skip}...${
     skip + limit
   }]{
     title,
@@ -48,6 +64,26 @@ export default async function BeritaPage({ searchParams }: PageProps) {
     },
     body
   }`;
+  const queryEN = `*[_type == 'newsEN'] | order(publishedAt desc) [${skip}...${
+    skip + limit
+  }]{
+    title,
+    slug,
+    publishedAt,
+    coverImage{
+      asset -> {url},
+      alt,
+    },
+    body
+  }`;
+
+  let query = queryEN;
+  if (locale == "id") {
+    query = queryID;
+  } else if (locale == "en") {
+    query = queryEN;
+  }
+
   const news = await client.fetch(query, { skip, limit });
   const t = await getTranslations("news");
   return (
